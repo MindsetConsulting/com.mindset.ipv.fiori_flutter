3a703cd16c5a1bcabed52b0227a0634f90080553
Curtis Kettler <curtiskettler@mindsetconsulting.com>
2023-08-11 11:36:34 -0500

    listview page development

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,201 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: ListReportPage(),
+    );
+  }
+}
+
+class ListReportPage extends StatelessWidget {
+  const ListReportPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          'List Report',
+          style: TextStyle(
+            fontFamily: 'SAP72',
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.add,
+              size: 32,
+              color: Colors.blue,
+            ),
+            onPressed: () {
+              // Add button functionality here
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              style: TextStyle(
+                fontFamily: 'SAP72',
+                fontStyle: FontStyle.italic,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Search',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Colors.grey[300],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.separated(
+              itemCount: 10,
+              separatorBuilder: (BuildContext context, int index) {
+                return Divider(
+                  color: Colors.grey[300],
+                  thickness: 1,
+                  height: 0,
+                );
+              },
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 16,
+                  ),
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.blue,
+                    child: Text(
+                      '$index',
+                      style: TextStyle(
+                        fontFamily: 'SAP72',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Item $index',
+                        style: TextStyle(
+                          fontFamily: 'SAP72',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Description of item $index',
+                        style: TextStyle(
+                          fontFamily: 'SAP72',
+                        ),
+                      ),
+                      Text(
+                        'Additional information about item $index',
+                        style: TextStyle(
+                          fontFamily: 'SAP72',
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  trailing: IconButton(
+                    iconSize: 20,
+                    icon: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.grey[300],
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ObjectDetailPage(index: index),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ObjectDetailPage extends StatelessWidget {
+  final int index;
+
+  const ObjectDetailPage({Key? key, required this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Row(
+            children: [
+              Icon(
+                Icons.arrow_back_ios,
+                color: Colors.blue,
+              ),
+              Text(
+                'Back',
+                style: TextStyle(
+                  fontFamily: 'SAP72',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          'Object $index Details',
+          style: TextStyle(
+            fontFamily: 'SAP72',
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.add,
+              size: 32,
+              color: Colors.blue,
+            ),
+            onPressed: () {
+              // Add button functionality here
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'Details for Object $index',
+          style: TextStyle(
+            fontFamily: 'SAP72',
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
         ),
       ),
     );
